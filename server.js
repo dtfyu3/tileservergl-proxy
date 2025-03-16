2ed9ff493d42e5eb4a265049a6c2268f63d635cf
Maxim Bogdanov <126229962+dtfyu3@users.noreply.github.com>
2025-03-16 10:09:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,52 +5,11 @@ const cache = new NodeCache({ stdTTL: 3600 }); // Кэш на 1 час
 const path = require('path');
 const { spawn } = require('cross-spawn');
 const fs = require('fs');
+require('dotenv').config();
 
 const app = express();
 
-const configPath = path.join(__dirname, 'tileserver', 'config.json');
-console.log(`Путь к config.json: ${configPath}`);
-
-
-if (!fs.existsSync(configPath)) {
-    console.error(`Файл config.json не найден по пути: ${configPath}`);
-    process.exit(1);
-}
-
-const mbtilesPath = path.join(__dirname, 'tileserver', 'localized_with_temps.mbtiles');
-if (!fs.existsSync(mbtilesPath)) {
-    console.error(`Файл localized_with_temps.mbtiles не найден по пути: ${mbtilesPath}`);
-    process.exit(1);
-}
-
-// app.use((req, res, next) => {
-//     console.log(`${req.method} ${req.url}`);
-//     next();
-// });
-const tileserverProcess = spawn(
-    'npx',
-    [
-        'tileserver-gl',
-        '--config',
-        path.join(__dirname, 'tileserver', 'config.json')
-    ],
-    { stdio: 'inherit' }
-);
-tileserverProcess.on('spawn', () => {
-    console.log('tileserver-gl запущен');
-})
-tileserverProcess.stdout?.on('data', (data) => {
-    console.log(`tileserver-gl: ${data}`);
-});
-
-tileserverProcess.stderr?.on('data', (data) => {
-    console.error(`tileserver-gl error: ${data}`);
-});
-
-tileserverProcess.on('close', (code) => {
-    console.log(`tileserver-gl завершил работу с кодом ${code}`);
-});
-
+const proxyUrl = process.env.PROXY_URL
 app.use('/tiles', (req, res, next) => {
     const cacheKey = req.originalUrl;
     const cachedResponse = cache.get(cacheKey);
@@ -60,7 +19,7 @@ app.use('/tiles', (req, res, next) => {
         res.send(cachedResponse.body);
     } else {
         createProxyMiddleware({
-            target: 'http://localhost:8080',
+            target: proxyUrl,
             changeOrigin: true,
             pathRewrite: { '^/tiles': '' },
             onProxyRes: (proxyRes, req, res) => {
